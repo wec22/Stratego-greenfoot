4e29af416059572ae271bd0f926406b56d3fe13b
amfurno <32942151+amfurno@users.noreply.github.com>
2017-10-31 16:19:06 -0500

    scout outline

--- Stratego/Scout.java
+++ Stratego/Scout.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Miner here.
+ * Write a description of class Scout here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Miner extends Piece
+public class Scout extends Piece
 {
     /**
-     * Act - do whatever the Miner wants to do. This method is called whenever
+     * Act - do whatever the Scout wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Miner()
+     public Scout()
     {
     }
-    public Miner(int rank, boolean color)
+    public Scout(int rank, boolean color)
     {
         super(rank, color);
     }
