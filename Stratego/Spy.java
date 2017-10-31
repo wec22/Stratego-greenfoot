044c598d8d96fe3e8b09d1be7ac9646d58e06d6e
amfurno <32942151+amfurno@users.noreply.github.com>
2017-10-31 16:18:23 -0500

    spy outline

--- Stratego/Spy.java
+++ Stratego/Spy.java
@@ -12,6 +12,13 @@ public class Spy extends Piece
      * Act - do whatever the Spy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+     public Spy()
+    {
+    }
+    public Spy(int rank, boolean color)
+    {
+        super(rank, color);
+    }
     public void act() 
     {
         // Add your action code here.
