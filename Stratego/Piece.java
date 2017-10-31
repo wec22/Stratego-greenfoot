d20f236612119094b63b992aaf460890cdb4f74f
amfurno <32942151+amfurno@users.noreply.github.com>
2017-10-31 16:15:45 -0500

    create piece outline
    creates the outline of methods in piece class

--- Stratego/Piece.java
+++ Stratego/Piece.java
@@ -12,8 +12,24 @@ public class Piece extends Actor
      * Act - do whatever the Piece wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int rank;
+    private boolean color;
+    public Piece()
+    {
+    }
+    public Piece(int number, boolean side)
+    {
+        rank = number;
+        color = side;
+    }
     public void act() 
     {
         // Add your action code here.
-    }    
+    }
+    public void move()
+    {
+    }
+    public void fight()
+    {
+    }
 }
