643c1ac938c96cc0637d760cb6ee9befb9206269
amfurno <hockeyamf@gmail.com>
2017-11-02 16:29:03 -0500

    add pieces for each side
    adds pieces for each side at a random locations

--- Stratego/MyWorld.java
+++ Stratego/MyWorld.java
@@ -13,6 +13,9 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    //temporary array that fills up with randomly placed soldiers
+    private int orderedPair;
+    private boolean SpawnLocationBoard[][] = new boolean[10][4];
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -23,24 +26,155 @@ public class MyWorld extends World
          * new pieceType(pieceNumber, Color)
          * true is red
          * false is blue
-         */
-        addObject(new Flag(11, true), 5, 1);
+         --------------*/
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Flag(11, true), (orderedPair/100), (orderedPair%100));//adds flag
 
-        for(int i= 15; i<85; i+=10)
+        for(int i= 0; i < 6; i++)//add bombs
         {
-            addObject(new Bomb(0, true), i, 1);            
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Bomb(0, true), (orderedPair/100), (orderedPair%100));            
         }
         
-        addObject(new Spy(1, true), 8, 1);
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Spy(10, true), (orderedPair/100), (orderedPair%100)); //add spy
         
-        for(int i = 9; i<17; i++)
+        for(int i = 0; i < 8; i++) //add scouts
         {
-            addObject(new Scout(2, true), i, 1);
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Scout(9, true),(orderedPair/100), (orderedPair%100));
         }
         
-        for(int i = 17; i<22; i++)
+        for(int i = 0; i < 5; i++) // add miners
         {
-            addObject(new Miner(3, true), i, 1);
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Miner(8, true), (orderedPair/100), (orderedPair%100));
         }
+        
+        for(int i = 0; i < 4; i++)// add sergents
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(7, true), (orderedPair/100), (orderedPair%100));
+        }
+        
+        for(int i = 0; i < 4; i++) // add lieutenant
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(6, true), (orderedPair/100), (orderedPair%100));
+        }
+        
+        for(int i = 0; i < 4; i++) // add captain
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(5, true), (orderedPair/100), (orderedPair%100));
+        }
+        
+        for(int i = 0; i < 3; i++) // add major
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(4, true), (orderedPair/100), (orderedPair%100));
+        }
+        
+        for(int i = 0; i < 2; i++) //add colonel
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(3, true), (orderedPair/100), (orderedPair%100));
+        }
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Piece(2, true), (orderedPair/100), (orderedPair%100)); //add General
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Piece(1, true), (orderedPair/100), (orderedPair%100)); //add Field Marshel
+        
+        
+        //creating blue---------------------------------------------
+        //----------------------------------------------------------
+        
+         SpawnLocationBoard = new boolean[10][4]; //reset the board to false
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Flag(11, false), (orderedPair/100), (orderedPair%100)+60);//adds flag
+        
+
+        for(int i= 0; i < 6; i++)//add bombs
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Bomb(0, false), (orderedPair/100), (orderedPair%100)+60);            
+        }
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Spy(10, false), (orderedPair/100), (orderedPair%100)+60); //add spy
+        
+        for(int i = 0; i < 8; i++) //add scouts
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Scout(9, false),(orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 5; i++) // add miners
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Miner(8, false), (orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 4; i++)// add sergents
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(7, false), (orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 4; i++) // add lieutenant
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(6, false), (orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 4; i++) // add captain
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(5, false), (orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 3; i++) // add major
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(4, false), (orderedPair/100),(orderedPair%100)+60);
+        }
+        
+        for(int i = 0; i < 2; i++) //add colonel
+        {
+            orderedPair = getRandomLocationPair(SpawnLocationBoard);
+            addObject(new Piece(3, false), (orderedPair/100), (orderedPair%100)+60);
+        }
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Piece(2, false), (orderedPair/100), (orderedPair%100)+60); //add General
+        
+        orderedPair = getRandomLocationPair(SpawnLocationBoard);
+        addObject(new Piece(1, false), (orderedPair/100), (orderedPair%100)+60); //add Field Marshel
+        
     }
+    public int getRandomLocationPair(boolean[][] board)
+    {
+        int xLocation;
+        int yLocation;
+        int orderedPair;
+        while(true)
+        {
+            xLocation=Greenfoot.getRandomNumber(10);
+            yLocation = Greenfoot.getRandomNumber(4);
+            
+            if(board[xLocation][yLocation] != true)
+            {
+                board[xLocation][yLocation] = true;
+                xLocation = (xLocation * 10) + 5; //changes from single digit location of array to 2 digit location in board grid
+                yLocation = (yLocation * 10) + 5;
+                orderedPair = (xLocation * 100)+yLocation; //combines the x and y location into 1 value to be passed back
+                return orderedPair;
+            }
+        }
+        
+    }
+   
 }
