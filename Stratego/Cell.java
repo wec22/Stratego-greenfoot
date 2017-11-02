78909621ece4ad060d2486cef509f932a9e9b897
wec22 <elicanup@aol.com>
2017-11-02 15:58:25 -0500

    added Cell class

---
+++ Stratego/Cell.java
@@ -0,0 +1,21 @@
+/**
+ * Write a description of class Cell here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cell  
+{
+    // instance variables - replace the example below with your own
+    public int x;
+    public int y;
+
+    /**
+     * Constructor for objects of class Cell
+     */
+    public Cell(int nx, int ny)
+    {
+        x = nx;
+        y = ny;
+    }
+}
