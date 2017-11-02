ecb8e73c7bbf1b12a7689cab00af90fe26c95557
wec22 <elicanup@aol.com>
2017-11-02 15:58:10 -0500

    added turns to the world

--- Stratego/MyWorld.java
+++ Stratego/MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private boolean turn;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +17,13 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(100, 100, 10); 
+    }
+    public void changeTurns()
+    {
+        turn = !turn;
+    }
+    public boolean getTurn()
+    {
+        return turn;
     }
 }
